5aebd3221180d63dae58066eaea826637413ad21
kharnagy <kharnagy@inedo.com>
2015-05-26 17:06:47 -0400

    Provider editor

--- JiraProviderEditor.cs
+++ JiraProviderEditor.cs
@@ -1,6 +1,6 @@
 ﻿using System.Web.UI.WebControls;
+using Inedo.BuildMaster;
 using Inedo.BuildMaster.Extensibility.Providers;
-using Inedo.BuildMaster.Web.Controls;
 using Inedo.BuildMaster.Web.Controls.Extensions;
 using Inedo.Web.Controls;
 
@@ -10,35 +10,24 @@ namespace Inedo.BuildMasterExtensions.Jira
     {
         private ValidatingTextBox txtUserName, txtBaseUrl, txtRelativeServiceUrl;
         private PasswordTextBox txtPassword;
-        private DropDownList ddlConsiderResolvedStatusAsClosed;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="JiraProviderEditor"/> class.
-        /// </summary>
-        public JiraProviderEditor()
-        {
-        }
+        private CheckBox chkConsiderResolvedStatusAsClosed;
 
         public override void BindToForm(ProviderBase extension)
         {
-            this.EnsureChildControls();
-
             var provider = (JiraProvider)extension;
             this.txtUserName.Text = provider.UserName;
             this.txtPassword.Text = provider.Password;
-            if (!string.IsNullOrEmpty(provider.RelativeServiceUrl))
-                this.txtRelativeServiceUrl.Text = provider.RelativeServiceUrl;
+            this.txtRelativeServiceUrl.Text = provider.RelativeServiceUrl;
             this.txtBaseUrl.Text = provider.BaseUrl;
         }
+
         public override ProviderBase CreateFromForm()
         {
-            this.EnsureChildControls();
-
             return new JiraProvider
             {
                 UserName = txtUserName.Text,
                 Password = txtPassword.Text,
-                RelativeServiceUrl = txtRelativeServiceUrl.Text,
+                RelativeServiceUrl = Util.CoalesceStr(txtRelativeServiceUrl.Text, "rpc/soap/jirasoapservice-v2"),
                 BaseUrl = txtBaseUrl.Text,
             };
         }
@@ -46,55 +35,24 @@ namespace Inedo.BuildMasterExtensions.Jira
         protected override void CreateChildControls()
         {
             this.txtUserName = new ValidatingTextBox();
-            
-            this.txtBaseUrl = new ValidatingTextBox
-            {
-                Width = Unit.Pixel(300)
-            };
-            
-            this.txtRelativeServiceUrl = new ValidatingTextBox
-            { 
-                Text = "rpc/soap/jirasoapservice-v2",
-                Width = Unit.Pixel(300)
-            };
-            
+            this.txtBaseUrl = new ValidatingTextBox();
+            this.txtRelativeServiceUrl = new ValidatingTextBox { DefaultText = "rpc/soap/jirasoapservice-v2" };
             this.txtPassword = new PasswordTextBox();
-            
-            this.ddlConsiderResolvedStatusAsClosed = new DropDownList();
-            ddlConsiderResolvedStatusAsClosed.Items.Add("Yes");
-            ddlConsiderResolvedStatusAsClosed.Items.Add("No");
+            this.chkConsiderResolvedStatusAsClosed = new CheckBox { Text = "Treat Resolved status as Closed", Checked = true };
 
             this.Controls.Add(
-                new FormFieldGroup("Jira Server URL",
-                    "The URL of the Jira server, for example: http://jiraserver:8080",
-                    false,
-                    new StandardFormField(
-                        "Server URL:",
-                        this.txtBaseUrl)
-                    ),
-                new FormFieldGroup("Jira Web Service Path",
-                    "The relative path to the Jira service URL. Generally, this will be \"rpc/soap/jirasoapservice-v2\"", /*http://jiraserver:8080/rpc/soap/jirasoapservice-v2*/
-                    false,
-                    new StandardFormField(
-                        "Service Path:",
-                        this.txtRelativeServiceUrl)
-                    ),
-                new FormFieldGroup("Authentication",
-                    "Provide a username and password to connect to the Jira service. The given user must be in both <b>jira-developers</b> and <b>jira-users</b> groups.",
-                    false,
-                    new StandardFormField(
-                        "User Name:",
-                        this.txtUserName),
-                    new StandardFormField(
-                        "Password:",
-                        this.txtPassword),
-                    new StandardFormField(
-                        "Consider Resolved Status as Closed?",
-                        this.ddlConsiderResolvedStatusAsClosed)
-                    )
+                new SlimFormField("JIRA server URL:", this.txtBaseUrl)
+                {
+                    HelpText = "The base URL of the JIRA server, e.g. http://jira:8080/"
+                },
+                new SlimFormField("Web service relative path:", this.txtRelativeServiceUrl),
+                new SlimFormField("User name:", this.txtUserName)
+                {
+                    HelpText = HelpText.FromHtml("This user will be used to authenticate to the JIRA web service. This user must be in both the <b>jira-developers</b> and <b>jira-users</b> groups.")
+                },
+                new SlimFormField("Password:", this.txtPassword),
+                new SlimFormField("Options:", this.chkConsiderResolvedStatusAsClosed)
             );
-
-            base.CreateChildControls();
         }
     }
 }
