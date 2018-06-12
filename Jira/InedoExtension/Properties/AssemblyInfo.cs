04394398abd5701edf26eaab36ecb59159a9a31f
jrasch <jrasch@users.noreply.github.com>
2018-06-12 10:11:13 -0400

    Update AssemblyInfo.cs

--- Jira/InedoExtension/Properties/AssemblyInfo.cs
+++ Jira/InedoExtension/Properties/AssemblyInfo.cs
@@ -2,10 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("JIRA")]
-[assembly: AssemblyDescription("Issue Tracking integration for JIRA 4 and later.")]
+[assembly: AssemblyDescription("Issue Tracking integration for JIRA 5 and later.")]
 [assembly: AssemblyCompany("Inedo, LLC")]
 [assembly: AssemblyProduct("any")]
 [assembly: AssemblyCopyright("Copyright © Inedo 2018")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("6.0.0")]
+[assembly: AssemblyFileVersion("6.0.0")]
 [assembly: ComVisible(false)]
